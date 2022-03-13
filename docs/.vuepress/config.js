ccbb3b9e76b2f250fecd074e321144ae25d0f1d6
admin <45819421+CXPhoenix@users.noreply.github.com>
2022-03-13 13:37:53 +0800

    init blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const path = require("path");
+
+const mdFile = ".md";
+const rootFolder = path.dirname(__dirname);
+const rootfiles = fs.readdirSync(rootFolder);
+const folders = rootfiles.filter(
+  (item) => path.extname(item) != mdFile && item != ".vuepress"
+);
+
+module.exports = {
+  title: "",
+  description: "Vuepress 模板",
+  themeConfig: {
+    logo: "/images/logoImage.png",
+    navbar: [
+      {
+        text: "教學筆記",
+        link: "/edu-note/",
+      },
+    ],
+    sidebar: { ...getSideBar() },
+  },
+};
+
+function getNavBar() {
+  const navbar = [];
+  folders.forEach((folder) => {
+    navbar.push({
+      text: folder.toUpperCase(),
+      link: `/${folder}/`,
+    });
+  });
+  return navbar;
+}
+
+function getSideBar() {
+  const sidebar = {};
+  folders.forEach((folder) => {
+    sidebar[`/${folder}/`] = [];
+    const folderFiles = fs.readdirSync(path.join(rootFolder, folder));
+    const children = [];
+    folderFiles
+      .filter((item) => item.toLowerCase() != "readme.md")
+      .forEach((file) => {
+        children.push(`/${folder}/${file}`);
+      });
+    sidebar[`/${folder}/`].push({
+      text: folder.toUpperCase(),
+      children: [`/${folder}/`, ...children],
+    });
+  });
+  return sidebar;
+}

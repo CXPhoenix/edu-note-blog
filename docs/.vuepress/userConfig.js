8d66f6cfa25f1b51484a1ed1702c0aa27b6bb520
CXPhoenix <jate9229@gmail.com>
2022-03-28 20:18:31 +0800

    update exclusive list

--- docs/.vuepress/userConfig.js
+++ docs/.vuepress/userConfig.js
@@ -1,6 +1,6 @@
 const title = "資教文本";
 const description = "關於資訊、教育、生活等等作筆記";
 const logo = "/images/logoImage.png";
-const exclusive_note = [];
+const exclusive_note = ["test-exclusive_note.md"];
 
 module.exports = { title, description, logo, exclusive_note };

f31d086fce0443d3260973e3712bca64b14b8ee0
CXPhoenix <jate9229@gmail.com>
2022-03-28 20:21:17 +0800

    fixed conditioning

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -47,7 +47,7 @@ function getSideBar() {
         (item) =>
           item.toLowerCase() != "readme.md" &&
           path.extname(item) === mdFile &&
-          exclusive_note.indexOf(item.toLowerCase) === -1
+          exclusive_note.indexOf(item) === -1
       )
       .forEach((file) => {
         children.push(`/${folder}/${file}`);
